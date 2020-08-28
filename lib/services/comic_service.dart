52742dd9f27c8e07e62c15d218ce160dd85a0f14
EL yisus <jmagamez50@gmail.com>
2020-08-28 08:21:35 -0500

    se agrego el limite al buscar las sugerencias

--- lib/services/comic_service.dart
+++ lib/services/comic_service.dart
@@ -28,9 +28,9 @@ class ComicService{
     }
   }
 
-    Future<List<Comic>> getFilterComics(String search) async{
+  Future<List<Comic>> getFilterComics(String search) async{
     try {
-      final response = await _dio.get('/volumes/?api_key=${this._apiKey}&filter=name:$search&sort=date_added:desc&format=json');
+      final response = await _dio.get('/volumes/?api_key=${this._apiKey}&limit=100&filter=name:$search&sort=date_added:desc&format=json');
       if(response.statusCode == 200){
         var comic = comicModelFromJson(response.data);
         return comic.results;
